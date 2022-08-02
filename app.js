eedbd975a6d7ffdc982126ac6ae881959b4fcd7d
vamsi <vamsikorrapolu123@gmail.com>
2022-08-02 11:58:22 +0530

    change

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
     credentials:true
 }))
